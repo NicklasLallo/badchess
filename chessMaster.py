9768d980cd6722527a95b42b55511dcd7adc3c67
Nicklas Lallo <lallo@student.chalmers.se>
2019-08-29 19:53:36 +0000

    logging can be disabled

--- chessMaster.py
+++ chessMaster.py
@@ -12,7 +12,7 @@ from ast import literal_eval as make_tuple
 
 class chessMaster:
 
-    def __init__(self, agentA, agentB, chessVariant='Standard'):
+    def __init__(self, agentA, agentB, log=True, chessVariant='Standard'):
 
         if not chessVariant == 'Standard':
             pass
@@ -22,33 +22,30 @@ class chessMaster:
         # gamelog = chess.pgn.Game()
         # gamelog.headers["Event"] = "Bot Championships Alpha"
 
-        with open('previousGame.pgn', 'w') as f:
-            active = True
-            while(not self.board.is_game_over()):
-                if active:
-                    movelist = agentA.makeMove(self.board, self.board.legal_moves)
-                else:
-                    movelist = agentB.makeMove(self.board, self.board.legal_moves)
-                active = not active
+        if log:
+            with open('previousGame.pgn', 'w') as f:
+                self.play(agentA, agentB)
+                gamelog = chess.pgn.Game.from_board(self.board)
+                gamelog.headers["Event"] = "Bot Champtionships Alpha"
+                f.write(str(gamelog))
+                f.close()
+        else:
+            self.play(agentA, agentB)
 
+    def play(self, agentA, agentB):
+        active = True
+        while(not self.board.is_game_over()):
+            if active:
+                movelist = agentA.makeMove(self.board, self.board.legal_moves)
+            else:
+                movelist = agentB.makeMove(self.board, self.board.legal_moves)
+            active = not active
 
-                #todo logic
-                for move in movelist:
-                    if move in self.board.legal_moves:
-                        self.board.push(move)
-                        break
-
-
-                # gamelog.add_variation(self.board.peek())
-                # f.write(self.board.peek().uci() + '\n')
-
-            # f.write(str(gamelog))
-            gamelog = chess.pgn.Game.from_board(self.board)
-            gamelog.headers["Event"] = "Bot Champtionships Alpha"
-            f.write(str(gamelog))
-            # f.write(self.board.result())
-            f.close()
-
+            #todo logic
+            for move in movelist:
+                if move in self.board.legal_moves:
+                    self.board.push(move)
+                    break
 
     def output(self):
         return str(self.board)+'\n\n'+self.board.result()
