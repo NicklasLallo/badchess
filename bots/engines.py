37d8c85c8eae622f950b652009e9ec7f0d08e1dd
Nicklas Lallo <nicklas@lallo.se>
2019-10-03 10:57:51 +0200

    Makes it possible to play against stockfish

---
+++ bots/engines.py
@@ -0,0 +1,27 @@
+import asyncio
+import chess
+import chess.pgn
+import chess.engine
+import numpy as np
+import time
+import random
+from bots.simple import chessBot
+
+
+class stockfish(chessBot):
+    def __init__(self):
+        self.engine = chess.engine.SimpleEngine.popen_uci("/usr/local/bin/stockfish")
+        # self.engine = await chess.engine.SimpleEngine.popen_uci("/usr/local/bin/stockfish")
+        # asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+
+    def quit(self):
+        self.engine.quit()
+
+    def makeMove(self, board, moves, verbose):
+        result = self.engine.play(board, chess.engine.Limit(time=0.100))
+        # board.push(result.move)
+        return [result.move]
+
+    def evalPos(self, board):
+        info = self.engine.analyse(board, chess.engine.Limit(time=0.100))
+        return int(info["score"])
